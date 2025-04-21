1f49249155c614404adb28e885e996221fe99e19
Udeesha rukshan <76590372+UdeeshaRukshan@users.noreply.github.com>
2025-04-21 18:34:17 +0530

    Create swift-classes.swift

---
+++ swift-classes.swift
@@ -0,0 +1,23 @@
+class Student {
+    var studentName: String
+    var studentAge: Int
+    var studentGender: String
+
+    init(studentName: String,
+         studentAge: Int,
+         studentGender: String)
+    {
+        self.studentName   = studentName
+        self.studentAge    = studentAge
+        self.studentGender = studentGender
+    }
+
+    func displayInfo() {
+        print("Student Name: \(studentName)")
+        print("Age: \(studentAge)")
+        print("Gender: \(studentGender)")
+    }
+}
+
+let student1 = Student(studentName: "Alice", studentAge: 20, studentGender: "Female")
+student1.displayInfo()
